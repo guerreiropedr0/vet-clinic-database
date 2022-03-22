5f64bc631f6103216324eebd7363ae22a3dde079
Pedro Guerreiro <guerreiropedr0@outlook.com>
2022-03-22 14:13:45 +0000

    Fix query not showing owners without animals

--- queries.sql
+++ queries.sql
@@ -136,23 +136,24 @@ WHERE species.id = 1;
 
 /*List all owners and their animals, remember to include those that don't own any animal*/
 
-SELECT * FROM animals
-LEFT JOIN owners ON animals.owner_id = owners.id;
+SELECT * FROM owners
+LEFT JOIN animals ON animals.owner_id = owners.id;
 
 /*
- id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species_id | owner_id | id |    full_name    | age
-----+------------+---------------+-----------------+----------+-----------+------------+----------+----+-----------------+-----
-  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 |          2 |        1 |  1 | Sam Smith       |  34
-  2 | Gabumon    | 2018-11-15    |               2 | t        |         8 |          2 |        2 |  2 | Jennifer Orwell |  19
-  3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 |          1 |        2 |  2 | Jennifer Orwell |  19
-  6 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 |          2 |        3 |  3 | Bob             |  45
-  4 | Devimon    | 2017-05-12    |               5 | t        |        11 |          2 |        3 |  3 | Bob             |  45
-  7 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 |          1 |        4 |  4 | Melody Pond     |  77
-  5 | Charmander | 2020-02-08    |               0 | f        |        11 |          1 |        4 |  4 | Melody Pond     |  77
- 10 | Blossom    | 1998-10-13    |               3 | t        |        17 |          1 |        4 |  4 | Melody Pond     |  77
-  8 | Angemon    | 2005-06-12    |               1 | t        |        45 |          2 |        5 |  5 | Dean Winchester |  14
-  9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 |          2 |        5 |  5 | Dean Winchester |  14
-(10 rows)
+ id |    full_name    | age | id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species_id | owner_id
+----+-----------------+-----+----+------------+---------------+-----------------+----------+-----------+------------+----------
+  1 | Sam Smith       |  34 |  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 |          2 |        1
+  2 | Jennifer Orwell |  19 |  2 | Gabumon    | 2018-11-15    |               2 | t        |         8 |          2 |        2
+  2 | Jennifer Orwell |  19 |  3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 |          1 |        2
+  3 | Bob             |  45 |  6 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 |          2 |        3
+  3 | Bob             |  45 |  4 | Devimon    | 2017-05-12    |               5 | t        |        11 |          2 |        3
+  4 | Melody Pond     |  77 |  7 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 |          1 |        4
+  4 | Melody Pond     |  77 |  5 | Charmander | 2020-02-08    |               0 | f        |        11 |          1 |        4
+  4 | Melody Pond     |  77 | 10 | Blossom    | 1998-10-13    |               3 | t        |        17 |          1 |        4
+  5 | Dean Winchester |  14 |  8 | Angemon    | 2005-06-12    |               1 | t        |        45 |          2 |        5
+  5 | Dean Winchester |  14 |  9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 |          2 |        5
+  6 | Jodie Whittaker |  38 |    |            |               |                 |          |           |            |
+(11 rows)
 */
 
 /*How many animals are there per species?*/
